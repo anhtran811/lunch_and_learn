b618d563715c1ff28f6ad75cb0acbc8d185c8be3
Anhnhi Tran <anhtran811@gmail.com>
2023-03-04 14:22:45 -0700

    Completed poro and facade for recipes by country

---
+++ spec/poros/recipe_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Recipe do
+  it 'exists and has attributes' do
+    country = "Thailand"
+    recipes = RecipeFacade.country_recipe(country)
+
+    attributes = {
+                  label: "Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)",
+                  url: "www.recipe-url.com",
+                  image: "www.image-url.com"
+                }
+
+    recipe = Recipe.new(country, attributes)
+
+    expect(recipe.title).to eq("Andy Ricker's Naam Cheuam Naam Taan Piip (Palm Sugar Simple Syrup)")
+    expect(recipe.url).to eq("www.recipe-url.com")
+    expect(recipe.image).to eq("www.image-url.com")
+    expect(recipe.country).to eq("Thailand")
+  end
+end
