8eb0bc9b1fd0a2a120d09b34d6ae2f9b0d00f92c
Anhnhi Tran <anhtran811@gmail.com>
2023-03-04 16:04:01 -0700

    Completed request for random country and if country parameter is empty

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    render json: RecipeSerializer.new(RecipeFacade.country_recipe(params[:country]))
+    if params[:country]
+      render json: RecipeSerializer.new(RecipeFacade.country_recipe(params[:country]))
+    else
+      country = CountryFacade.random_country
+      render json: RecipeSerializer.new(RecipeFacade.country_recipe(country.name))
+    end
   end
 end
--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -2,22 +2,20 @@ require 'rails_helper'
 
 describe 'GET /recipes' do
   context 'when the country is inputted' do
-    it 'can return a response with recipes from that country' do
+    it 'can return a response with recipes from that country', :vcr do
       country = "thailand"
       get "/api/v1/recipes?country=#{country}"
 
       expect(response).to be_successful
 
-      country_response = JSON.parse(response.body, symbolize_names: true)
+      recipe_response = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_successful
+      expect(recipe_response).to be_a(Hash)
 
-      expect(country_response).to be_a(Hash)
+      expect(recipe_response).to have_key(:data)
+      expect(recipe_response[:data]).to be_an(Array)
 
-      expect(country_response).to have_key(:data)
-      expect(country_response[:data]).to be_an(Array)
-
-      data = country_response[:data][0]
+      data = recipe_response[:data][0]
 
       expect(data).to have_key(:id)
       expect(data[:id]).to be_nil
@@ -45,4 +43,58 @@ describe 'GET /recipes' do
       expect(data[:attributes]).to_not have_key(:healthLabels)
     end
   end
+
+  context 'if the country is not inputted by user' do
+    it 'can return recipes for a random country', vcr: { record: :new_episodes } do
+      get "/api/v1/recipes"
+
+      expect(response).to be_successful
+
+      recipe_response = JSON.parse(response.body, symbolize_names: true)
+      expect(recipe_response).to be_a(Hash)
+
+      expect(recipe_response).to have_key(:data)
+      expect(recipe_response[:data]).to be_an(Array)
+
+      data = recipe_response[:data][0]
+
+      expect(data).to have_key(:id)
+      expect(data[:id]).to be_nil
+      
+      expect(data).to have_key(:type)
+      expect(data[:type]).to eq("recipe")
+
+      expect(data).to have_key(:attributes)
+      expect(data[:attributes]).to be_a(Hash)
+
+      expect(data[:attributes]).to have_key(:title)
+      expect(data[:attributes][:title]).to be_a(String)
+
+      expect(data[:attributes]).to have_key(:url)
+      expect(data[:attributes][:url]).to be_a(String)
+
+      expect(data[:attributes]).to have_key(:country)
+      expect(data[:attributes][:country]).to be_a(String)
+
+      expect(data[:attributes]).to have_key(:image)
+      expect(data[:attributes][:image]).to be_a(String)
+
+      expect(data[:attributes]).to_not have_key(:uri)
+      expect(data[:attributes]).to_not have_key(:source)
+      expect(data[:attributes]).to_not have_key(:healthLabels)
+    end
+  end
+
+  context 'if the user leaves the country parameter empty' do
+    it 'returns an empty array' do
+      get "/api/v1/recipes?country="
+
+      expect(response).to be_successful
+
+      recipe_response = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipe_response).to have_key(:data)
+      expect(recipe_response[:data]).to eq([])
+    end
+  end 
 end
