a56fa7ada591e18556a03e1fcb98ea7b014c2216
Anhnhi Tran <anhtran811@gmail.com>
2023-03-06 11:37:03 -0700

    Fixed test in tourist sight poro

--- spec/poros/tourist_sight_spec.rb
+++ spec/poros/tourist_sight_spec.rb
@@ -11,8 +11,19 @@ RSpec.describe TouristSight do
     expect(tourist_sights[0]).to be_a(TouristSight)
     expect(tourist_sights).to be_an(Array)
 
-    expect(tourist_sights[0].name).to eq("Crédit Foncier de France")
-    expect(tourist_sights[0].address).to eq("Crédit Foncier de France, Rue des Capucines, Paris, France")
-    expect(tourist_sights[0].place_id).to eq("51d03648d27fa0024059d4741e11536f4840f00103f9013aa86933010000009203194372c3a964697420466f6e63696572206465204672616e6365")
+    attributes = { 
+                    properties: 
+                              {
+                                name: "Crédit Foncier de France",
+                                formatted: "Crédit Foncier de France, Rue des Capucines, Paris, France",
+                                place_id: "1235rgn93tq34i"
+                            }
+                  }
+
+    sight = TouristSight.new(attributes)
+
+    expect(sight.name).to eq("Crédit Foncier de France")
+    expect(sight.address).to eq("Crédit Foncier de France, Rue des Capucines, Paris, France")
+    expect(sight.place_id).to eq("1235rgn93tq34i")
   end
 end
