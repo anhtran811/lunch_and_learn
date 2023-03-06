a5d355c2bbe44f656d03d6204e16f440a3d0b81f
Anhnhi Tran <anhtran811@gmail.com>
2023-03-06 13:56:33 -0700

    Corrected error handling error from learning resources sad path

--- app/facades/photo_facade.rb
+++ app/facades/photo_facade.rb
@@ -1,10 +1,8 @@
 class PhotoFacade
   def self.get_photos(country)
     photos = UnsplashService.get_photos_by_country(country)
-    # require 'pry'; binding.pry
-    if photos[0][:errors]
-      error = []
-      return error
+    if photos.class == Hash
+      return []
     else
       photos.map do |photo|
         Photo.new(photo)
