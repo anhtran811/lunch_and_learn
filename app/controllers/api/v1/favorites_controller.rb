ac2e24c96e742c8b82a75130d017ef478945692e
Anhnhi Tran <anhtran811@gmail.com>
2023-03-07 14:01:25 -0700

    Refactor favorites controller to use before_action to find user

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :find_user, only: [:index, :create]
+  
   def create
-    if user = User.find_by(api_key: params[:favorite][:api_key])
+    if @user
+      user = User.find_by(api_key: params[:api_key])
       user.favorites.create!(favorite_params)
       render json: { "success": "Favorite added successfully" }, status: 201
     else
@@ -9,8 +12,9 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   def index
-    if user = User.find_by(api_key: params[:api_key])
-      favorites = user.favorites
+    if @user
+      user = User.find_by(api_key: params[:api_key])
+      favorites = @user.favorites
       render json: FavoritesSerializer.new(favorites), status: 200
     else
       render json: { "error": "Unable to find user"}, status: 400
@@ -19,7 +23,11 @@ class Api::V1::FavoritesController < ApplicationController
 
   private
 
+  def find_user
+    @user = User.find_by(api_key: params[:api_key])
+  end
+
   def favorite_params
-    params.require(:favorite).permit(:country, :recipe_link, :recipe_title)
+    params.permit(:country, :recipe_link, :recipe_title)
   end
 end
