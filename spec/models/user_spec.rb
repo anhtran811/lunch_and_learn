f46f8f6085593c07cda8aa8be7f9a09ce745a831
Anhnhi Tran <anhtran811@gmail.com>
2023-03-06 17:06:18 -0700

    Updated model validations and relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:api_key) }
+  describe 'relationships' do
+    it { should have_many(:favorites) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    # it { should validate_presence_of(:api_key) }
+    # it { should validate_uniqueness_of(:api_key).ignoring_case_sensitivity}
+  end
 
   describe 'instance methods' do
     it 'should generate an api key' do
